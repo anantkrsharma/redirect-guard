843f4bc26912aed13900e8711c6d3fec07d97fee
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-12-27 00:54:59 +0530

    configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 import { resolve as _resolve } from "path";
 
-export const mode = "development";
 export const entry = {
     background: "./src/background.ts",
-    popup: "./src/popup.ts",
-    rules: "./src/rules.ts"
+    content: "./src/content.ts",
+    popup: "./src/popup/index.tsx"
 };
 export const output = {
     filename: "[name].js",
     path: _resolve(__dirname, "dist")
 };
+export const resolve = {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+};
 export const module = {
     rules: [
         {
-            test: /\.ts$/,
-            use: "ts-loader",
+            test: /\.tsx?$/,
+            loader: "ts-loader",
             exclude: /node_modules/
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
         }
     ]
-};
-export const resolve = {
-    extensions: [".ts", ".js"]
 };
